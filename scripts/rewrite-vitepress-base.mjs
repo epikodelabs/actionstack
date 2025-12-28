8a111c2d665b3f9f585345b974ae3a5665020f53
actioncrew <sepeloleksij4@gmail.com>
2025-12-28 07:50:01 +0200

    corrected vitepress script

--- scripts/rewrite-vitepress-base.mjs
+++ scripts/rewrite-vitepress-base.mjs
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const distRoot = path.join(process.cwd(), 'docs/.vitepress/dist');
-const basePath = '/actionstack';
+const basePath = '/actionstack/';
 const baseSegment = basePath.replace(/^\/|\/$/g, '');
 
 function processDirectory(dir) {
