b329b1ad180e4f08dae078a9c756e309a5913483
actioncrew <sepeloleksij4@gmail.com>
2025-12-28 13:50:07 +0200

    set utf-8 as encoding for all pages

--- vitepress.config.ts
+++ vitepress.config.ts
@@ -117,6 +117,7 @@ export default defineConfig({
   },
 
   head: [
+    ['meta', { charset: 'utf-8' }],
     ['link', { rel: 'icon', href: '/actionstack/favicon.ico' }],
     ['meta', { name: 'theme-color', content: '#3c82f6' }],
     ['meta', { name: 'og:type', content: 'website' }],
