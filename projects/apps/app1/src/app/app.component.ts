24d833cc110b6d782a0e2254830680aa31d9c789
actioncrew <sepeloleksij4@gmail.com>
2026-03-10 18:52:31 +0200

    corrections

--- projects/apps/app1/src/app/app.component.ts
+++ projects/apps/app1/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import type { OnDestroy, OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import type { Subscription } from 'rxjs/internal/Subscription';
+
+import { MessagesModule } from './messages/messages.module';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  standalone: false
+  standalone: true,
+  imports: [RouterModule, MessagesModule]
 })
 export class AppComponent implements OnInit, OnDestroy {
   title = 'Tour of Heroes';
--- projects/apps/app1/src/main.ts
+++ projects/apps/app1/src/main.ts
@@ -1,7 +1,11 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
 
-import { AppModule } from './app/app.module';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(AppRoutingModule)]
+})
   .catch(err => console.error(err));
 
