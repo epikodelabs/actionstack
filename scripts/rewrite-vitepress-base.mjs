e6f22f0a67d7e09ff08c233856092549e20fb37f
actioncrew <sepeloleksij4@gmail.com>
2026-04-08 14:21:23 +0300

    corrected build script

--- scripts/rewrite-vitepress-base.mjs
+++ scripts/rewrite-vitepress-base.mjs
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const distRoot = path.join(process.cwd(), 'docs/.vitepress/dist');
+const distRoot = path.join(process.cwd(), 'dist/.vitepress/dist');
 const basePath = '/actionstack/';
 
 function processDirectory(dir) {
